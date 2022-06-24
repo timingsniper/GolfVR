05a900a8087f147b63cba798e829c0a3d7f48381
TheWitness2020 <67212928+TheWitness2020@users.noreply.github.com>
2022-06-24 15:43:42 +0800

    出界复活和时间变色闪烁

--- Assets/Scripts/TimeSpeed.cs
+++ Assets/Scripts/TimeSpeed.cs
@@ -6,27 +6,44 @@ using static GameCore_Data;
 
 public class TimeSpeed : MonoBehaviour
 {
+    Vector3 temp;
     Text info;
     Rigidbody rb;
-    float tim=0;
-    int min=0, v;
+    float tim = 0, spd = 0.7f, rec = 1;
+    Color col = Color.blue;
+    int min = 0, v;
     bool updated = false;
     void Start()
     {
+        temp = pl.transform.position;
         rb = pl.GetComponent<Rigidbody>();
         info = tx.GetComponent<Text>();
         tx.SetActive(true);
     }
     void Update()
     {
+        if(pl.transform.position.y < -50)
+        {
+            pl.transform.position = temp;
+            pl.transform.rotation = new Quaternion(0, 0, 0, 1);
+            rb.velocity = Vector3.zero;
+            v = 0;
+            return;
+        }
         int s = (int)(tim * 10);
         if (finished)
         {
             if (!updated)
             {
-                info.text = $"<color=red>{min / 10}{min % 10}:{s / 100}{s / 10 % 10}.{s % 10}   !</color>";
+                if (min == 0 && s < 400) { col = Color.green; spd = 1.2f; }
+                if (min == 0 && s < 300) { col = Color.cyan; spd = 1.7f; }
+                if (min == 0 && s < 270) { col = Color.red; spd = 2.2f; }
+                info.text = $"{min / 10}{min % 10}:{s / 100}{s / 10 % 10}.{s % 10}   !";
                 updated = true;
             }
+            rec += spd * Time.deltaTime; if (rec >= 2) rec -= 2;
+            col.a = (rec <= 1 ? rec : 2 - rec);
+            info.color = col;
             return;
         }
         tim += Time.deltaTime;
@@ -35,7 +52,7 @@ public class TimeSpeed : MonoBehaviour
         v = (int)rb.velocity.magnitude;
         s = (int)(tim * 10);
         info.text = $"{min / 10}{min % 10}:{s / 100}{s / 10 % 10}.{s % 10}      {v / 100}.{v / 10 % 10}{v % 10}m/s";
-        au1.GetComponent<AudioSource>().volume = v / 800.0f;
+        au1.GetComponent<AudioSource>().volume = v / 700.0f;
         if (Mathf.Abs(tv - v) > 40)
         {
             au2.GetComponent<AudioSource>().volume = (Mathf.Abs(tv - v) - 40) / 800.0f;
